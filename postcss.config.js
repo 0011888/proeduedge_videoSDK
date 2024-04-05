a05c9df3f5c0653cb52309566b1f1c45882bee30
Farrukh Ruzmetov <farrukhruzmetov2002@gmail.com>
2024-04-05 16:14:11 +0500

    configuration changed

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-
-export default {
-  plugins: [
-    autoprefixer,
-    tailwindcss,
-    // other plugins
-  ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
